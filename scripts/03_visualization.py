3f4b06eceea492dd1c2aabfcef23614eeb8f204f
BenjaminChan <benjamin.v.chan@gmail.com>
2025-05-12 23:33:51 -0700

    03_visualization script outline

---
+++ scripts/03_visualization.py
@@ -0,0 +1,5 @@
+# 1. Load cleaned data.
+# 2. Create histograms of Tuition_USD and Rent_USD.
+# 3. Create boxplot of Living_Cost_Index by Country (top 10).
+# 4. Create bar chart of average tuition by Level.
+# 5. Save all figures to outputs folder.
